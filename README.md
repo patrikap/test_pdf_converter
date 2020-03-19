cf7a3fab44eb8be3d76ea603884f227b471f68d8
Konstantin <patrikap@gmail.com>
2020-03-19 20:41:57 +0300

    # create to_pdf package

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# test_pdf_converver
-Test task
+# test_pdf_converter
+Solution for test task.
+
+## Install
+Install `a2ps` and `ps2pdf` packages:
+
+```bash
+sudo apt-get install a2ps t2html html2ps ghostscript
+```
+
+Clone repo:
+
+```bash
+clone git@github.com:patrikap/test_pdf_converter.git ~/pdf_converter
+```
+
+Allow execute file
+
+```bash
+chmod +x ~/pdf_converter/to_pdf.sh
+```
+ 
+Create symlink to `/usr/local/bin` 
+
+```bash
+sudo ln -s ~/pdf_converter/to_pdf.sh /usr/bin/to_pdf
+```
+ 
+## Using
+
+```bash
+to_pdf *path_to_files*
+```
+
+### Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information what has changed recently.
+
+# Author
+ 
+[Patrikap](https://github.com/patrikap)
---
+++ to_pdf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+### DIRECTORIES
+PROJECT_PATH=`pwd`
+LOG_FILE="$PROJECT_PATH/$0.log"
+### WRITE LOG
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NORMAL='\033[0m'
+function log {
+    DTM=`date  +%H:%M:%S`
+    if [[ $2 -eq 0 ]];then
+        echo -e "${DTM}\t${1}\t\t${GREEN}OK${NORMAL}"
+    else
+        echo -e "${DTM}\t${1}\t\t${RED}ERROR${NORMAL}" 1>&2
+    fi
+    echo "${DTM}    ${1}" >> ${LOG_FILE}
+}
+### CONVERT FUNCTION
+function convert {
+    if [[ -f "$1" ]] ; then
+        FILE=$1
+        a2ps ${FILE} --silent -g -o - | ps2pdf - ${FILE}.pdf
+        log "Created a new .pdf file on path $FILE.pdf"
+    else
+        log "First parameter must be valid file ($FILE)" 1
+    fi
+}
+### CHECK SRC
+if [[ -d "$1" ]] ; then
+    DATA_DIR="$1"
+    log "Directory $DATA_DIR" $?
+else
+    log "First parameter 'src' must be valid directory on your filesystem." 1
+    exit 1
+
+fi
+### RECURSIVE SEARCH
+FILES=`find ${DATA_DIR} -type f -name '*.php' -o -name '*.html' -o -name '*.js'`
+for FILE in ${FILES[*]}
+do
+    log "File found on path $FILE" $?
+    convert ${FILE}
+done
+exit 0
